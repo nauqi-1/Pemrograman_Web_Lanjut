b16d7813a61491eaafafbd2309404978e776e56e
nauqi-1 <nauqi2208@gmail.com>
2024-09-02 10:01:23 +0700

    Update Percobaan 1 Jobsheet 2

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function() {
+    return 'Hello World';
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +15,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/world', function () {
+    return 'World';
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang!';
+});
+
+Route::get('/about', function() {
+    return 'Nama saya Muhammad Naufal Assyauqi Ardyawan <br> NIM saya adalah 2241760046';
+});
+
+Route::get('/user/{name?}', function($name='John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/article/{id}', function ($id) {
+    return 'Halaman artikel dengan ID ' . $id;
 });
