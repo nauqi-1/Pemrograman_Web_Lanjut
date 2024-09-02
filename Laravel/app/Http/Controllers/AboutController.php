c55aa964755190a93a2b3831a4d0b4d89d8aaf0a
nauqi-1 <nauqi2208@gmail.com>
2024-09-02 11:34:31 +0700

    Upload Praktikum 2 Jobsheet 2

---
+++ Laravel/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama saya Muhammad Naufal Assyauqi Ardyawan <br> NIM saya adalah 2241760046';
+    }
+}
